be134d8eec5b52b2f5da3b847baaa34856b38358
nadzeya <vitserresvit@gmail.com>
2023-05-17 19:08:51 +0300

    new

---
+++ src/main/java/homeWork2/charWork.java
@@ -0,0 +1,18 @@
+package homeWork2;
+
+public class charWork {
+    public static void main(String[] args) {
+        char a = '4';
+        int result = Integer.parseInt(String.valueOf(a));
+
+        System.out.println(result + 6);
+
+        char b = '4';
+
+        double result1 = Character.getNumericValue(b);
+        System.out.println(result1 + 6);
+
+
+
+    }
+}
